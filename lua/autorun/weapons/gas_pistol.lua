8ff585721ed316deaab8cb8e7faf575687181b24
rwilliaise <rwilliaise@users.noreply.github.com>
2021-12-12 18:37:40 -0800

    Add GAS PISTOL

---
+++ lua/autorun/weapons/gas_pistol.lua
@@ -0,0 +1,21 @@
+SWEP.PrintName			= "marty_gas_pistol"
+
+SWEP.Spawnable			= true
+SWEP.UseHands			= true
+
+SWEP.ViewModel			= "models/weapons/c_pistol.mdl"
+SWEP.WorldModel			= "models/weapons/w_pistol.mdl"
+
+SWEP.Author				= "Marty"
+SWEP.Instructions		= "GAS..."
+SWEP.Category			= "Marty's Weapons"
+
+SWEP.Base				= "weapon_base"
+
+function SWEP:Reload()
+	-- no reload
+end
+
+function SWEP:Think()
+	-- no thinking
+end
