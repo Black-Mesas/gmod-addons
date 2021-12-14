a92af0d0b686ebb8fca0489b4b0d31c0f4ede378
rwilliaise <rwilliaise@users.noreply.github.com>
2021-12-13 18:22:10 -0800

    feat: Make gross hack to add a context menu

--- lua/autorun/client/cl_blackmesas.lua
+++ lua/autorun/client/cl_blackmesas.lua
@@ -1,4 +1,24 @@
 
-print("Hey. :)")
+-- gross hack to make stuff work :)
+spawnmenu.AddContentType = function(name, func)
+    if name ~= "entity" then
+        spawnmenu.AddContentType(name, func)
+        return
+    end
+
+    func = function(container, obj)
+        local out = func(container, obj)
+        out.OpenMenuExtra = function(self, menu)
+            (out.OpenMenuExtra or function() end)(self, menu)
+
+            menu:AddOption( "Spawn with Clone Wand", function()
+                print("This is a test!")
+            end):SetIcon( "icon16/brick_add.png" )
+        end
+
+        return out
+    end
+    spawnmenu.AddContentType(name, func)
+end
 
 
--- lua/weapons/thky_clone_stick/init.lua
+++ lua/weapons/thky_clone_stick/init.lua
@@ -13,41 +13,41 @@ local Sounds = include("include/sounds.lua")
 
 function Tool:PrimaryAttack()
 	local Owner = self:GetOwner()
-	
+
 	Timer = Timer + 1
-	
+
 	if Timer > 5 then
 		if Target[Owner] then
 			local Location = Owner:GetShootPos() + (Owner:GetAimVector() * 75)
 			local Velocity = Owner:GetAimVector() * 2000
 			local Angle = Owner:EyeAngles()
-			
+
 			if Extra.IsInWorld(Location) then
 				local Entity = Duplicator.CreateEntityFromTable(Owner,Target[Owner])
 				local Physics = Entity:GetPhysicsObject()
-				
+
 				if Physics:IsValid() then
 					Physics:EnableMotion(true)
-					
+
 					Physics:SetPos(Location)
 					Physics:SetVelocity(Velocity)
 					Physics:SetAngles(Angle)
 					Physics:Wake()
 				end
-				
+
 				History.Create("prop")
 				History.AddEntity(Entity)
 				History.SetPlayer(Owner)
 				History.Finish("Clone Wand " .. tostring(Entity))
 
 				Owner:AddCleanup("props", Entity)
-				
+
 				Owner:EmitSound(Sounds.Fire)
 			else
 				Owner:EmitSound(Sounds.Fail)
 			end
 		end
-		
+
 		Timer = 0
 	end
 end
@@ -55,12 +55,12 @@ end
 function Tool:SecondaryAttack()
 	local Owner = self:GetOwner()
 	local Result = Ray(Owner:GetShootPos(),Owner:GetAimVector() * (256^3))
-	
+
 	if Result.Entity then
 		if Duplicator.IsAllowed(Result.Entity:GetClass()) then
 			Target[Owner] = Duplicator.CopyEntTable(Result.Entity)
 			Owner:EmitSound(Sounds.Select)
-			
+
 			if Result.Entity:GetPhysicsObject():IsValid() then
 				Owner:PrintMessage(HUD_PRINTCENTER,string.format("Selected %s",tostring(Result.Entity)))
 			else
@@ -74,7 +74,7 @@ end
 
 function Tool:Reload()
 	local Owner = self:GetOwner()
-	
+
 	if Target then
 		Target[Owner] = nil
 		Owner:EmitSound(Sounds.Clear)
